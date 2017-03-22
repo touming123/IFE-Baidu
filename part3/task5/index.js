3274553f1059a72fec288eefe46e228a4b0e7b65
touming123 <junlijiaojiao@163.com>
2017-03-22 16:14:09 +0800

    task5

---
+++ part3/task5/index.js
@@ -0,0 +1,45 @@
+(function() {
+
+
+    function init() {
+        document.getElementById('TRALEF').onclick = function() {
+            var square = document.getElementById('square');
+        }
+        document.getElementById('TRATOP').onclick = function() {
+                
+        }
+        document.getElementById('TRARIG').onclick = function() {
+                
+        }
+        document.getElementById('TRABOT').onclick = function() {
+                
+        }
+//
+        document.getElementById('MOVLEF').onclick = function() {
+                
+        }
+        document.getElementById('MOVTOP').onclick = function() {
+                
+        }
+        document.getElementById('MOVRIG').onclick = function() {
+                
+        }
+        document.getElementById('MOVBOT').onclick = function() {
+                
+        }
+//
+        document.getElementById('GO').onclick = function() {
+                
+        }
+        document.getElementById('TUNLEF').onclick = function() {
+                
+        }
+        document.getElementById('TUNRIG').onclick = function() {
+                
+        }
+        document.getElementById('TUNBAC').onclick = function() {
+                
+        }
+    }
+    init();
+})()
