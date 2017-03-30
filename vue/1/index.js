1080020e79999e020dd4f6630caadc3d744d17b6
touming123 <junlijiaojiao@163.com>
2017-03-30 10:53:15 +0800

    vue taks1

---
+++ vue/1/index.js
@@ -0,0 +1,44 @@
+function Observer(data) {
+    this.data = data;
+    this.walk(data);
+}
+let pro = Observer.prototype;
+pro.walk = function(obj) {
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            let val = obj[key];
+            if (typeof val === 'object') {
+                new Observer(val);
+            }
+            this.convert(key, val);
+        }
+    }
+};
+
+pro.convert = function(key, val) {
+    Object.defineProperty(this.data, key, {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            console.log('你访问了' + key);
+            return val;
+        },
+        set: function(newVal) {
+            console.log('你设置了'+ key);
+            console.log('新的' + key + '=' + newVal);
+            val = newVal;
+        }
+    })
+}
+
+
+let data = {
+    user: {
+        name: "liangshaofeng",
+        age: "24"
+    },
+    address: {
+        city: "beijing"
+    }
+};
+let app = new Observer(data);
